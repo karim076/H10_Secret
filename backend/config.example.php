f2d9310582472415412fbb74d43f4e83653fd2e0
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-17 14:24:11 +0100

    Relocating naar mijn mappen path

--- backend/config.example.php
+++ backend/config.example.php
@@ -12,4 +12,4 @@ $dbUser = 'root';
 $dbPass = '';
 
 //De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/H4-secret';
+$base_url = 'http://localhost/Tweede%20Periode/H10_secret';
